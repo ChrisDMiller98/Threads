25ee6af62d710f23c1aa6861eb2adb60476302b9
Taco <tacoface1214@yahoo.com>
2020-03-16 14:59:12 -0500

    Made most of FindPiThread

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -10,14 +10,25 @@ namespace Threads
     {
         public FindPiThread(int nDarts)
         {
-
+            numDarts = nDarts;
+            dartCount = 0;
+            rand = new Random();
+            rand.Next();
         }
         int numDarts;
         int dartCount;
         Random rand;
         public void throwDarts()
         {
+            for(int i = 0; i < numDarts; i++)
+            {
 
+            }
+            //Inside this function, you should have a loop 
+            //that randomly “throws” every dart it should 
+            //throw.  After determining the x and y 
+            //coordinates, increment your counter of 
+            //how many land inside!
         }
     }
 
@@ -25,6 +36,18 @@ namespace Threads
     {
         static void Main(string[] args)
         {
+            //Put accessor here?????
+            Console.WriteLine("How many throws should each thread make?");
+            int numThrows;
+            numThrows = Console.Read();
+            Console.WriteLine("How many threads would you like to run?");
+            int numThreads;
+            numThreads = Console.Read();
+
+            Loop1{
+                FindPiThread FPT();
+
+            }
         }
     }
 }
